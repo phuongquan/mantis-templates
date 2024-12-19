3d6d23f648645e1e91f0ed5a36422a515b85a631
Phuong Quan <phuong.Quan@ukhsa.gov.uk>
2024-12-19 12:36:04 +0000

    basic template

---
+++ main.R
@@ -0,0 +1,35 @@
+# run this whole file to generate all the template reports
+
+#---------------------------------------------------
+# SETUP
+
+install.packages(c("remotes", "rmarkdown", "dplyr", "reactable"))
+remotes::install_github("phuongquan/mantis", upgrade = FALSE)
+
+save_directory <- "./output"
+
+if (!file.exists(save_directory)){
+  stop(paste0("save_directory [", save_directory, "] does not exist"))
+}
+
+#---------------------------------------------------
+# GET SOME DATA
+
+# use example dataset provided with mantis
+example_prescription_numbers <- mantis::example_prescription_numbers
+
+# data manipulation can be done either to the dataset before calling render() and/or inside the Rmd 
+
+#---------------------------------------------------
+# GENERATE A BASIC REPORT
+
+rmarkdown::render(
+  input = "templates/basic_template.Rmd",
+  output_file = "basic_template", 
+  output_dir = save_directory,
+  params = list(
+    df = example_prescription_numbers
+  )
+)
+
+
